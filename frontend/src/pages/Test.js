a6b28c4f42712595b42ebbe2ef4a8d4ec308497f
Xyron Brual <xyronb@gmail.com>
2022-11-24 17:56:19 -0500

    basic setup for test page
    -need to separate into components and set functionality

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -2,16 +2,66 @@ import React, { useContext, useEffect, useState } from "react";
 import AuthContext from "../context/AuthContext";
 import { API_URL } from "../constants";
 import { useNavigate } from "react-router-dom";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Row,
+  Card,
+  Button,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
 
 // Test page where users test their typing speed
 export default function Test(props) {
+  // context data
   let { user, authTokens } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  // state val for retrieved data from API URL
   let [quote, setQuote] = useState({});
+
+  // state val for div after quote processed
+  let [quoteDiv, setQuoteDiv] = useState();
+
+  // inputted state value from user input
   let [typed, setTyped] = useState("");
 
+  // state value for test -> intial value value is before
+  let [testState, setTestState] = useState("before");
+
+  // state values for recording time taken
+  let [times, setTimes] = useState({});
+
+  // state values for the final results fo the test
+  let [result, setResult] = useState({});
+
+  // implement immediate change in quoteDiv state
+  const changeQuoteDiv = (arr) => {
+    setQuoteDiv((current) => arr);
+  };
+
+  // function handles what to do when button is clicked
+  const buttonClick = () => {
+    if (testState === "before") {
+      setTestState("during");
+    }
+    if (testState === "during") {
+      setTestState("after");
+    }
+    if (testState === "after") {
+      resetTest();
+    }
+    console.log(testState);
+  };
+
+  // function resets the test  page's state values
+  const resetTest = () => {
+    getQuote();
+    setTestState("before");
+  };
+
+  // function uses API to retrieve a new quote
   const getQuote = async () => {
     console.log("Getting Quote");
     let url = API_URL + "/random-quote";
@@ -27,17 +77,32 @@ export default function Test(props) {
     if (response.status === 200) {
       setQuote(data);
       console.log(data);
+      extractQuoteDiv(data.text);
     } else {
       console.log(data);
       navigate("/");
     }
   };
 
+  // function uses quote value to make a div and set it
+  const extractQuoteDiv = (text) => {
+    let arr = text.split("").map((value) => {
+      return <span className="quote-char">{value}</span>;
+    });
+
+    // setQuoteDiv(arr);
+    // setQuoteDiv({ ...quoteDiv, div: arr });
+    // console.log(arr);
+    changeQuoteDiv(arr);
+  };
+
+  // called when user input changes
   const handleInput = (e) => {
     const input = e.target.value;
     setTyped(input);
   };
 
+  // function to compare the quote to the inputted text
   const compareText = (input) => {};
 
   useEffect(() => {
@@ -48,11 +113,72 @@ export default function Test(props) {
 
   return (
     <Container>
-      {quote.text}
-      <br />
-      <input type="text" onChange={handleInput}></input>
-      <br />
-      {typed}
+      <Card>
+        <Card.Body>
+          <Row>
+            <Col>Source: {quote.source}</Col>
+            <Col>{quote.quotee}</Col>
+            <Col>{quote.tags}</Col>
+          </Row>
+          <Row>
+            <div>{quoteDiv}</div>
+          </Row>
+          <Row>
+            <InputGroup>
+              <Form.Control
+                as="textarea"
+                aria-label="With textarea"
+                onChange={handleInput}
+                // maxLength=""
+              />
+            </InputGroup>
+          </Row>
+          {/* <input type="text" onChange={handleInput}></input> */}
+          {/* {typed} */}
+          {/* {testButton(testState)} */}
+          <TestButton state={testState} buttonClick={buttonClick} />
+        </Card.Body>
+      </Card>
+      <ResultsCard speed={10} time={10} accuracy={20} />
     </Container>
+  );
+}
+
+function TestButton({ state, buttonClick }) {
+  switch (state) {
+    case "before":
+      return (
+        <Button variant="primary" onClick={buttonClick}>
+          Start
+        </Button>
+      );
+    case "during":
+      return <Button onClick={buttonClick}>Testing...</Button>;
+    case "after":
+      return (
+        <Button variant="primary" onClick={buttonClick}>
+          Play Again
+        </Button>
+      );
+  }
+}
+
+function ResultsCard({ speed, time, accuracy }) {
+  return (
+    <Card>
+      <Card.Body>
+        <Row>
+          <Col className="d-flex justify-content-md-center">
+            Speed: {speed} wpm
+          </Col>
+          <Col className="d-flex justify-content-md-center">
+            Time taken: {time}
+          </Col>
+          <Col className="d-flex justify-content-md-center">
+            Accuracy: {accuracy}
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
   );
 }
