4520e0d54e5c10029f2b75872aceae3dfdb2b722
Xyron Brual <xyronb@gmail.com>
2023-09-13 14:13:28 -0400

    Added api url as env variable

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000/api"
+// const API_URL = "http://127.0.0.1:8000/api"
+const API_URL = process.env.REACT_APP_API_URL
 
 // Constants used in components
 export { API_URL };
