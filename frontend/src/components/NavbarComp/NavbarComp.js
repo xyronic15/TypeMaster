a7e4e8f67fe347cc8fb0490a6e599bc99967340a
Xyron Brual <xyronb@gmail.com>
2023-09-26 19:20:04 -0400

    Completed styling the navbar

---
+++ frontend/src/components/NavbarComp/NavbarComp.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { IoPersonCircleSharp } from "react-icons/io5";
+import "./NavbarComp.css"
+import AuthContext from "../../context/AuthContext";
+
+// Navbar component on the top of every page
+const NavbarComp = (props) => {
+  let { user, logout } = useContext(AuthContext);
+  return (
+    <div>
+      <Navbar expand="md" variant="dark" className="mb-4">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={process.env.PUBLIC_URL + "/logos/tm_logo_hori_mono.png"}
+              className="d-inline-block align-top"
+              height="40"
+              alt="TypeMaster"
+            />
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            {/* Common links */}
+            <Nav className="ms-auto">
+              <Nav.Link href="/test">Test</Nav.Link>
+              <Nav.Link href="/high-scores">High Scores</Nav.Link>
+
+              {/* Adjust links based on whether user is logged in or not */}
+              {user ? (
+                <NavDropdown title={
+                  <>
+                    Welcome {user.username}
+                    <IoPersonCircleSharp size={25} className="ms-1 d-inline-block align-top" />
+                  </>} className="me-2">
+                  <NavDropdown.Item href="/records">Dashboard</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={logout}>Sign Out</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <>
+                  <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+                  <Nav.Link href="sign-in">Sign In</Nav.Link>
+                </>
+              )}
+            </Nav>
+
+            {/* Adjust links based on whether user is logged in or not */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+export default NavbarComp;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,4 @@
-import NavbarComp from "./NavbarComp";
+import NavbarComp from "./NavbarComp/NavbarComp";
 import RecordsTable from "./RecordsTable";
 
 // Export components that are used in multiple other components
